16542be4b8a00b23677917efc4a801f428436eae
Chinglew <pasin98@gmail.com>
2022-08-14 15:29:47 +0700

    test vue fucntion

--- ipuatsons/src/App.vue
+++ ipuatsons/src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <h1>v-model</h1>
+    <input v-model="email" type="email" placeholder="enter your fucking email">
+    <div>{{email}}</div>
+    <button @click="email=''">Reset</button>
+
+  </div>
 </template>
 
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      email:""
+    }
+  },
+  methods: {
+    
+  },
+
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
