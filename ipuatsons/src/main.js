f4998c94d4fae0e2145b72e9c3bfda33b3025d05
Chinglew <pasin98@gmail.com>
2023-03-01 11:42:30 +0700

    change JWT method

--- ipuatsons/src/main.js
+++ ipuatsons/src/main.js
@@ -3,16 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "./assets/style.css"
-import { globalCookiesConfig } from "vue3-cookies";
-
-
-globalCookiesConfig({
-    expireTimes: "5h",
-    path: "/",
-    domain: "",
-    secure: true,
-    sameSite: "None",
-  });
 
 createApp(App).use(store).use(router).mount('#app')
 
--- ipuatsons/src/views/ProductHistoryView.vue
+++ ipuatsons/src/views/ProductHistoryView.vue
@@ -1,8 +1,12 @@
 <template>
+
     <div>
         <SlideBar></SlideBar>
 
+
+
         <div class="main-content">
+
             <header>
                 <div class="menu-toggle">
                     <label for="sidebar-toggle">
@@ -12,140 +16,67 @@
                 <span class="bars"></span>
 
                 <div style="color:#000 ;">
-                    Market
+                    asd
                 </div>
             </header>
 
+
             <main>
-                <div class="page-header">
-                    <div>
-                        <h1>Marketplace</h1>
-                        <small>ตลาดซื้อ-ขาย</small>
-                    </div>
+                <div class="loading" v-if="this.isLoading">Loading&#8230;</div>
+                <h1>Drive page</h1>
 
-                    <div class="header-actions">
-                        <button @click="goToAddProduct">
-                            <span class="las la-plus"></span>
-                            Add product to sell
-                        </button>
-                    </div>
+                
+                <!--Add new folder image here  -->
+                <div style="padding:5px;">
+                    <button class="dropbtn" type="button" @click="addNewFolder" >Add new folder</button>
+                </div>
+                
+                
 
+
+                <!-- folder image list show here  -->
+                <div style="background:#e7e5e6">
+                    
+                    <ul style="padding:5px;">
+                        <div v-if="files < 1" 
+                        style="text-align: center;
+                            align-items: center;
+                            color: #000;
+                            background:#ccc;">!!! You do not have image folder !!!</div>
+
+                        <li v-for="file in files" v-bind:key="file.id" style="margin-top: 5px;margin-bottom: 5px; ">
+
+                            <div style="display : flex; 
+                             flex-direction : row;
+                             justify-content: space-between;
+                             align-items : center;
+                             padding-right: 15px;
+                             background:#ccc;">
+
+                                <div style="color:black;padding:10px; " @click="enterFolder(file.folder_id)">
+                                    {{ file.folder_name }}</div>
+
+                                <!-- dropUp list here  -->
+                                <div class="dropup">
+                                    <button class="dropbtn">Option</button>
+                                    <div class="dropup-content">
+                                        <a @click="deleteFolder(file.folder_id)">Delete</a>
+                                        <a href="#">Edit name</a>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </li>
+
+                    </ul>
                 </div>
 
 
-                <!-- show all products here -->
-                <div class="cards">
-
-
-                    <!-- show each product info here -->
-                    <div class="card-single" @click="goToProduct(1)">
-                        <div class="card-flex">
-                            <div class="card-info">
-                                <div class="card-head">
-                                    <span style="color: #000;">Seller - </span>
-                                    <small style="color: #000;">Peachi_27</small>
-                                </div>
-                                <img src="https://live.staticflickr.com/65535/52639909358_84b98cc68f_o_d.jpg">
-                                <h2 style="color: #000;">Yha tub sean gun</h2>
-                                <small style="color: #000;">object detection</small>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end of show each product info here -->
-
-
-                    <!-- show each product info here -->
-                    <div class="card-single">
-                        <div class="card-flex">
-                            <div class="card-info">
-                                <div class="card-head">
-                                    <span style="color: #000;">Seller - </span>
-                                    <small style="color: #000;">Peachi_27</small>
-                                </div>
-                                <img src="https://live.staticflickr.com/65535/52282498076_acc417f003_o_d.jpg">
-                                <h2 style="color: #000;">Yha tub sean gun</h2>
-                                <small style="color: #000;">object detection</small>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end of show each product info here -->
-
-                    <!-- show each product info here -->
-                    <div class="card-single">
-                        <div class="card-flex">
-                            <div class="card-info">
-                                <div class="card-head">
-                                    <span style="color: #000;">Seller - </span>
-                                    <small style="color: #000;">Peachi_27</small>
-                                </div>
-                                <img src="https://live.staticflickr.com/65535/52282495158_e31d45500c_o_d.jpg" style="">
-                                <h2 style="color: #000;">Yha tub sean gun</h2>
-                                <small style="color: #000;">object detection</small>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end of show each product info here -->
-
-
-
-                    <div class="card-single">
-                        <div class="card-flex">
-                            <div class="card-info">
-                                <div class="card-head">
-                                    <span style="color: #000;">Coming Soon</span>
-                                    <small style="color: #000;">Coming Soon</small>
-                                </div>
-                                <h2 style="color: #000;">Coming Soon</h2>
-                                <small style="color: #000;">Coming Soon</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-single">
-                        <div class="card-flex">
-                            <div class="card-info">
-                                <div class="card-head">
-                                    <span style="color: #000;">Coming Soon</span>
-                                    <small style="color: #000;">Coming Soon</small>
-                                </div>
-                                <h2 style="color: #000;">Coming Soon</h2>
-                                <small style="color: #000;">Coming Soon</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-single">
-                        <div class="card-flex">
-                            <div class="card-info">
-                                <div class="card-head">
-                                    <span style="color: #000;">Coming Soon</span>
-                                    <small style="color: #000;">Coming Soon</small>
-                                </div>
-                                <h2 style="color: #000;">Coming Soon</h2>
-                                <small style="color: #000;">Coming Soon</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-single">
-                        <div class="card-flex">
-                            <div class="card-info">
-                                <div class="card-head">
-                                    <span style="color: #000;">Coming Soon</span>
-                                    <small style="color: #000;">Coming Soon</small>
-                                </div>
-                                <h2 style="color: #000;">Coming Soon</h2>
-                                <small style="color: #000;">Coming Soon</small>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-                </div>
             </main>
         </div>
-        <label for="sidebar-toggle" class="body-label"></label>
     </div>
 </template>
-
 <script>
 import SlideBar from '@/components/SlideBar'
 import { useCookies } from "vue3-cookies";
