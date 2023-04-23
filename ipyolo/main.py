67fa84550e60298a237c622c88f588cd5c4ba1b4
Sutee Saraphan <suteesaraphan27@gmail.com>
2023-04-23 16:50:35 +0700

    Update main.py

--- ipyolo/main.py
+++ ipyolo/main.py
@@ -141,36 +141,34 @@ async def detect_return_base64_img(job_id = "",useridparam="",folders = "",model
                                                                 {'job_status' : 1
                                                                 }
                                                             },upsert=True)
-            img_file_col = db['api_image_file']
+        img_file_col = db['api_image_file']
+        user_id = useridparam #user ownner of this image
+        img_folder = newfolder #folder name of image
+        folder_path = os.path.join("ipautsons", user_id, "root", img_folder) #path of image folder
+        path_for_add_db = img_folder.split("/")[-3:]
+        onlyfiles = listdir(folder_path)
+        list_add_db = []
+        for i in onlyfiles:
+            dict_temp = {}
+            img_type = "image/"+i.split('.')[-1]
+            file_size = os.path.getsize(folder_path+"/"+i)
 
+            dict_temp = {
+                        'img_id': ''.join(random.choices(string.ascii_lowercase + string.digits, k=6)),
+                        'user_id_id': user_id,
+                        'img_type': img_type,
+                        'img_folder': img_folder.split("/")[-1],
+                        'path': path_for_add_db[0]+"/"+path_for_add_db[1]+"/"+path_for_add_db[2]+"/"+i,
+                        'img_size': str(file_size)
+                        }
 
-            user_id = useridparam #user ownner of this image
-            img_folder = newfolder #folder name of image
-            folder_path = os.path.join("ipautsons", user_id, "root", img_folder) #path of image folder
-            path_for_add_db = img_folder.split("/")[-3:]
-            onlyfiles = listdir(folder_path)
-            list_add_db = []
-            for i in onlyfiles:
-                dict_temp = {}
-                img_type = "image/"+i.split('.')[-1]
-                file_size = os.path.getsize(folder_path+"/"+i)
+            list_add_db.append(dict_temp)
 
-                dict_temp = {
-                            'img_id': ''.join(random.choices(string.ascii_lowercase + string.digits, k=6)),
-                            'user_id_id': user_id,
-                            'img_type': img_type,
-                            'img_folder': img_folder.split("/")[-1],
-                            'path': path_for_add_db[0]+"/"+path_for_add_db[1]+"/"+path_for_add_db[2]+"/"+i,
-                            'img_size': str(file_size)
-                            }
-
-                list_add_db.append(dict_temp)
-
-            try:
-                result = img_file_col.insert_many(list_add_db)
-                print(result)
-            except Exception as error:
-                print(error)
+        try:
+            result = img_file_col.insert_many(list_add_db)
+            print(result)
+        except Exception as error:
+            print(error)
     except:
       if job != None and type(job) == dict:
         job = db.api_job.find_one_and_update({'job_id' : job_id},
