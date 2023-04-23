a50a726b77f7ef03e6c311d69081b6c63e98f59e
SuteeSaraphan <suteesaraphan27@gmail.com>
2023-04-23 15:01:55 +0700

    ganapi

---
+++ api/ga/main.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI, File, Response
+from typing import Optional
+import cv2
+from segmentation import get_image_from_bytes
+from engine import Engine
+from animegan import AnimeGAN
+import io
+from PIL import Image
+import numpy as np
+import json
+from collections import Counter
+import io
+from io import BytesIO
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
+
+
+
+app = FastAPI(
+    title="Ipautsons API GANModel Preview",
+    description="""Ipautsons API GANModel Preview""",
+    version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:4070",
+    "*"
+]
+
+
+@app.get("/")
+async def root():
+    return dict(msg='OK')
+
+
+@app.post("/ascii")
+async def convert_image_to_ascii(file: bytes = File(...),modelse = "models/Shinkai_53.onnx",):
+    input_image = get_image_from_bytes(file)
+    input_image = np.array(input_image)
+    animegan = AnimeGAN(modelse)
+    engine = Engine(image_path=input_image, show=False,  custom_objects=[animegan])
+    input_image = engine.run()
+
+    
+
+    pil_image = Image.fromarray(input_image)
+    watermark = Image.open("watermark.png")
+    width, height = pil_image.size
+    w_width, w_height = watermark.size
+    x = (width - w_width) // 2
+    y = (height - w_height) // 2
+    pil_image.paste(watermark, (x, y), watermark)
+
+    # save PIL image to file buffer
+    buffer = io.BytesIO()
+    pil_image.save(buffer, format="JPEG")
+    image_bytes = buffer.getvalue()
+
+    return StreamingResponse(io.BytesIO(image_bytes), media_type="image/jpeg")
+
---
+++ api/ga/utils.py
@@ -0,0 +1,63 @@
+import numpy as np
+import typing
+import time
+import cv2
+
+class FPSmetric:
+    """ Measure FPS between calls of this object
+    """
+    def __init__(
+        self, 
+        range_average: int = 30,
+        position: typing.Tuple[int, int] = (7, 70),
+        fontFace: int = cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale: int = 3,
+        color: typing.Tuple[int, int, int] = (100, 255, 0),
+        thickness: int = 3,
+        lineType: int = cv2.LINE_AA,
+        ):
+        """
+        Args:
+            range_average: (int) = 30 - number of how many call should be averaged for a result
+            position: (typing.Tuple[int, int]) = (7, 70) - position in a frame where to put text
+            fontFace: (int) = cv2.FONT_HERSHEY_SIMPLEX - cv2 font for text
+            fontScale: (int) = 3 - size of font
+            color: (typing.Tuple[int, int, int]) = (100, 255, 0) - RGB color for text
+            thickness: (int) = 3 - chickness for text
+            lineType: (int) = cv2.LINE_AA - text line type
+        """
+        self._range_average = range_average
+        self._frame_time = 0
+        self._prev_frame_time = 0
+        self._fps_list = []
+
+        self.position = position
+        self.fontFace = fontFace
+        self.fontScale = fontScale
+        self.color = color
+        self.thickness = thickness
+        self.lineType = lineType
+
+    def __call__(self, frame: np.ndarray = None) -> typing.Union[bool, np.ndarray]:
+        """Measure duration between each call and return calculated FPS or frame with added FPS on it
+
+        Args:
+            frame: (np.ndarray) - frame to add FPS text if wanted
+
+        Returns:
+            fps: (float) - fps number if frame not given otherwise return frame (np.ndarray)
+        """
+        self._prev_frame_time = self._frame_time
+        self._frame_time = time.time()
+        if not self._prev_frame_time:
+            return frame
+        self._fps_list.append(1/(self._frame_time - self._prev_frame_time))
+        self._fps_list = self._fps_list[-self._range_average:]
+        
+        fps = float(np.average(self._fps_list))
+
+        if frame is None:
+            return fps
+
+        cv2.putText(frame, str(int(fps)), self.position, self.fontFace, self.fontScale, self.color, self.thickness, self.lineType)
+        return frame
