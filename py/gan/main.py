e7ef30643fb774ed588820068105e79a047a9119
Sutee Saraphan <suteesaraphan27@gmail.com>
2023-04-25 13:52:38 +0700

    update script

--- py/gan/main.py
+++ py/gan/main.py
@@ -4,6 +4,8 @@ from animegan import AnimeGAN
 import sys
 import pymongo
 import random
+import PIL
+from PIL import Image
 import string
 import os
 from os import *
@@ -34,10 +36,15 @@ if __name__ == '__main__':
             print(x)
             animegan = AnimeGAN(ganmodel)
             filename, extension = os.path.splitext(os.path.basename(All_files[x]))
-            All_files[x].save(newfolder+"/"+filename+extension)
+            image = Image.open(All_files[x])
             newpath = newfolder+"/"+filename+extension
+            oldname = newfolder+"/"+filename+"_out"+extension
+            image.save(newpath)
             engine = Engine(image_path=newpath, show=False,  custom_objects=[animegan])
             engine.run()
+            os.remove(newpath)
+            os.rename(oldname, newpath)
+
         print("done")
         if job != None and type(job) == dict:
             job = db.api_job.find_one_and_update({'job_id' : job_id},
@@ -45,38 +52,36 @@ if __name__ == '__main__':
                                                                 {'job_status' : 1
                                                                 }
                                                             },upsert=True)
-            img_file_col = db['api_image_file']
+        img_file_col = db['api_image_file']
 
 
-            user_id = useridparam #user ownner of this image
-            img_folder = newfolder #folder name of image
-            folder_path = os.path.join("ipautsons", user_id, "root", img_folder) #path of image folder
+        user_id = useridparam #user ownner of this image
+        img_folder = newfolder #folder name of image
+        folder_path = os.path.join("ipautsons", user_id, "root", img_folder) #path of image folder
+        path_for_add_db = img_folder.split("/")[-3:]
+        onlyfiles = listdir(folder_path)
+        list_add_db = []
+        for i in onlyfiles:
+            dict_temp = {}
+            img_type = "image/"+i.split('.')[-1]
+            file_size = os.path.getsize(folder_path+"/"+i)
 
+            dict_temp = {
+                        'img_id': ''.join(random.choices(string.ascii_lowercase + string.digits, k=6)),
+                        'user_id_id': user_id,
+                        'img_type': img_type,
+                        'img_folder': img_folder.split("/")[-1],
+                        'path': path_for_add_db[0]+"/"+path_for_add_db[1]+"/"+path_for_add_db[2]+"/"+i,
+                        'img_size': str(file_size)
+                        }
 
-            onlyfiles = listdir(folder_path)
-            list_add_db = []
+            list_add_db.append(dict_temp)
 
-            for i in onlyfiles:
-                dict_temp = {}
-                img_type = "image/"+i.split('.')[-1]
-                file_size = os.path.getsize(folder_path+"/"+i)
-
-                dict_temp = {
-                            'img_id': ''.join(random.choices(string.ascii_lowercase + string.digits, k=6)),
-                            'user_id_id': user_id,
-                            'img_type': img_type,
-                            'img_folder': img_folder.split("/")[-1],
-                            'path': img_folder+"/"+i,
-                            'img_size': str(file_size)
-                            }
-
-                list_add_db.append(dict_temp)
-
-            try:
-                result = img_file_col.insert_many(list_add_db)
-                print(result)
-            except Exception as error:
-                print(error)
+        try:
+            result = img_file_col.insert_many(list_add_db)
+            print(result)
+        except Exception as error:
+            print(error)
     except Exception as error:
         print(error)
         if job != None and type(job) == dict:
